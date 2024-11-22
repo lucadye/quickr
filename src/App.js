a6cf90fb37541928c462a0a1aae091264b740532
lucadye <121118370+lucadye@users.noreply.github.com>
2024-11-22 18:04:18 -0500

    Integrate Rust into the project via WebAssembly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import init, { add } from "wasm";
 import './App.css';
 
 function App() {
+
+  const [ans, setAns] = useState(0);
+  useEffect(() => {
+    init().then(() => {
+      setAns(add(40, 2));
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Life = {ans}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
